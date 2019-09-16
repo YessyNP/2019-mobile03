a66dd9f77607a68315cecda26a177f516a3f5448
YessyNP <yessynindi@gmail.com>
2019-09-16 13:06:46 +0700

    Aplikasikuu

---
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/work_happines.java
@@ -0,0 +1,12 @@
+package id.ac.polinema.aplikasiku;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class work_happines extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_happiness);
+    }
+}
